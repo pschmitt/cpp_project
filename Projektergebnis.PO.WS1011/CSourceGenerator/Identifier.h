35c78058be77949f26bfe3baabe3ce3c13405206
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-12-03 01:20:00 +0100

    Merged identifiers

--- Projektergebnis.PO.WS1011/CSourceGenerator/Identifier.h
+++ Projektergebnis.PO.WS1011/CSourceGenerator/Identifier.h
@@ -4,9 +4,7 @@
 #define _IDENTIFIER_H_INCLUDED_
 
 #include <string>
-
 using namespace std;
-
 
 
 class Identifier 
@@ -15,13 +13,13 @@ class Identifier
 /* Funktionsdeklaration */
 public:
    
-
-  enum IdentifierType { VARIABLE, CONSTANT, TYPE, FUNCTION, DIRECTORY, UNKNOWN};
-  Identifier (const string& name, IdentifierType type = UNKNOWN, const string& _description = "") ;
+  enum IdentifierType { VARIABLE, CONSTANT, ENUM, CLASS, STRUCT, FUNCTION, PARAMETER, WORKSPACE, PROJECT, MODUL, UNKNOWN};
+ 
+  Identifier (const string& name, IdentifierType type = UNKNOWN, const string& _description = "") throw (std::string);
   
-	string getName() const { return name; }
-	string getDescription() const { return description; }
-	void setDescription(const string& _description) { description = _description; }
+	const string& getName() const { return name; }
+	const string& getDescription() const { return description; }
+	void setDescription(const string& description) { this->description = description; }
 /* Konstantendeklaration */
   
   
@@ -29,19 +27,25 @@ public:
   private:
           string name; 
           string description;
-           
-          bool isDigit(char c) const;
-          bool isLowerCase(char c) const;
-          bool isUpperCase(char c) const;
-          bool isUnderscore(char c) const; 
+           //creer une classe avec ces fctions
           
-          bool isConstantName (const string& name) const;
-          bool isVariableName (const string& name) const;
-          bool isClassName(const string& name) const;
-          bool isEnumName(const string& name) const;
-          bool isStructName(const string& name) const;
-          bool isFunctionName(const string& name) const;
-          bool isDirectoryName(const string& name) const;
+          static bool isDigit(char c);
+	      static bool isLowerCase(char c);
+	      static bool isUpperCase(char c);
+	      static bool isUnderscore(char c); 
+		  static bool nonFirstLetter(char c);
+          static bool isConstantName (const string& name);
+		  
+          static bool isVariableName (const string& name);
+          static bool isClassName(const string& name) ;
+          static bool isEnumName(const string& name);
+          static bool isStructName(const string& name) ;
+          static bool isFunctionName(const string& name) ;
+		  static bool isParameterName(const string& name) ;
+          static bool isWorkspaceName(const string& name) ;
+		  static bool isProjectName(const string& name);
+		  static bool isModulName(const string& name);
+		  static bool check(const string& name, bool (*f)(char), bool (*g)(char));
    
   
   
